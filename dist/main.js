c9b43921a62c6b40f0de6a3b8eb5d0fb92377468
Vimpandra <victorimperatriz@gmail.com>
2023-03-15 21:09:49 -0300

    Added 3 slides and auto sliding on a 5s timer

--- dist/main.js
+++ dist/main.js
@@ -7,26 +7,90 @@
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
 /******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/autoSliding.js":
+/*!****************************!*\
+  !*** ./src/autoSliding.js ***!
+  \****************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"autoSliding\": () => (/* binding */ autoSliding)\n/* harmony export */ });\nfunction autoSliding(onDisplay) {\n    setInterval(changeSlide, 5000);\n\n    function changeSlide() {\n        const slide1 = document.getElementById('slide1');\n        const slide2 = document.getElementById('slide2');\n        const slide3 = document.getElementById('slide3');\n\n        if (onDisplay === 'slide1') {\n            slide1.style.left = '-100vw';\n            slide2.style.left = '0';\n            slide3.style.left = '100vw';\n            onDisplay = 'slide2';\n        } else if (onDisplay === 'slide2') {\n            slide1.style.left = '-200vw';\n            slide2.style.left = '-100vw';\n            slide3.style.left = '0';\n            onDisplay = 'slide3';\n        } else if (onDisplay === 'slide3') {\n            slide1.style.left = '0';\n            slide2.style.left = '100vw';\n            slide3.style.left = '200vw';\n            onDisplay = 'slide1';\n        }\n    }\n}\n\n\n\n//# sourceURL=webpack://image-slider/./src/autoSliding.js?");
+
+/***/ }),
 
 /***/ "./src/index.js":
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
-/***/ (() => {
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("console.log(`hello, world`);\n\n\n//# sourceURL=webpack://image-slider/./src/index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _autoSliding__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./autoSliding */ \"./src/autoSliding.js\");\n\n\nlet onDisplay = 'slide1';\n\n(0,_autoSliding__WEBPACK_IMPORTED_MODULE_0__.autoSliding)(onDisplay);\n\n//# sourceURL=webpack://image-slider/./src/index.js?");
 
 /***/ })
 
 /******/ 	});
 /************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
 /******/ 	
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module can't be inlined because the eval devtool is used.
-/******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/index.js");
 /******/ 	
 /******/ })()
 ;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-console.log(`hello, world`);
+import { autoSliding } from "./autoSliding";
+
+let onDisplay = 'slide1';
+
+autoSliding(onDisplay);
